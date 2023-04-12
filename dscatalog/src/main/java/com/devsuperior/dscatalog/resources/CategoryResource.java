8b4146e3399e1dbbd0db63653548f8cd0ab4d671
andresilvaq <andresilvaq@gmail.com>
2023-04-11 23:32:58 -0300

    CategoryResource

--- dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.dscatalog.entities.Category;
 
 @RestController
-@RequestMapping (value = "/categories")
+@RequestMapping (value = "/categories") 
 public class CategoryResource {
 	 
 	@GetMapping
