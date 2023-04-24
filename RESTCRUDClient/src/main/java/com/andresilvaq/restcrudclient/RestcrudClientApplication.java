41ab1d020fd0b46a0977c9ced89a7e303971a04c
andresilvaq <andresilvaq@gmail.com>
2023-04-24 11:30:52 -0300

    Project RESTCRUDClient created, packages added

---
+++ RESTCRUDClient/src/main/java/com/andresilvaq/restcrudclient/RestcrudClientApplication.java
@@ -0,0 +1,13 @@
+package com.andresilvaq.restcrudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcrudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcrudClientApplication.class, args);
+	}
+
+}
---
+++ RESTCRUDClient/src/test/java/com/andresilvaq/RESTCRUDClient/RestcrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresilvaq.RESTCRUDClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcrudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
